a7f5be33a6ada29684495708e28e13933fed2c99
Carfagna Alessandro <81586392+YourKarfee@users.noreply.github.com>
2022-02-20 14:17:43 +0100

    Aggiunti tutti i commenti di spiegazione

--- Pulsante_a_Ritenuta_Arduino/Pulsante_a_Ritenuta_Arduino.ino
+++ Pulsante_a_Ritenuta_Arduino/Pulsante_a_Ritenuta_Arduino.ino
@@ -1,24 +1,32 @@
-//Definizione pin utilzzati per bottone e LED
+// Definizione pin utilzzati per bottone e LED
 #define BUT 2
 #define LED 3
 
-//Definizione degli stati del pulsante. Il primo di ritenuta, il secondo per memorizzare lo stato passato dello stesso
+// Definizione degli stati del pulsante. Il primo di ritenuta, il secondo per memorizzare lo stato passato dello stesso
 int    iBut = 0;
 int iButOld = 0;
 
 void setup()
 {
+  
+  // Definizione modalità di funzione dei pin
   pinMode(BUT,  INPUT);
   pinMode(LED, OUTPUT);
 }
 
 void loop()
 {
+  
+  // Attivazione della lettura digitale del pin del pulsante
   int sBut = digitalRead(BUT);
 
+  // Se lo stato memorizzato e lo stato attuale, nel momento in cui il pulsante venisse premuto, sono diversi. La variabile
+  // contenente lo stato del pulsante da utilizzare inverte il suo stato
   if (sBut != iButOld && sBut) iBut = !iBut;
 
+  // La memorizzazione del pulsante si aggiorna all'attuale
   iButOld = sBut;
 
+  // Il led assume lo stato di utilizzo del pulsante
   digitalWrite(LED, iBut);
 }
