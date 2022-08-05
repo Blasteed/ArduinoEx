40fd00add94e3bbc0b127209847cd73e175ede52
Carfagna Alessandro <81586392+YourKarfee@users.noreply.github.com>
2022-08-05 08:42:58 +0200

    Modificate parentesi

--- Pulsante_a_Ritenuta_Arduino/Pulsante_a_Ritenuta_Arduino.ino
+++ Pulsante_a_Ritenuta_Arduino/Pulsante_a_Ritenuta_Arduino.ino
@@ -6,27 +6,27 @@
 int    iBut = 0;
 int iButOld = 0;
 
-void setup()
+void setup ()
 {
   
   // Definizione modalità di funzione dei pin
-  pinMode(BUT,  INPUT);
-  pinMode(LED, OUTPUT);
+  pinMode ( BUT,  INPUT );
+  pinMode ( LED, OUTPUT );
 }
 
 void loop()
 {
   
   // Attivazione della lettura digitale del pin del pulsante
-  int sBut = digitalRead(BUT);
+  int sBut = digitalRead ( BUT );
 
   // Se lo stato memorizzato e lo stato attuale, nel momento in cui il pulsante venisse premuto, sono diversi. La variabile
   // contenente lo stato del pulsante da utilizzare inverte il suo stato
-  if (sBut != iButOld && sBut) iBut = !iBut;
+  if ( sBut != iButOld && sBut ) iBut = !iBut;
 
   // La memorizzazione del pulsante si aggiorna all'attuale
   iButOld = sBut;
 
   // Il led assume lo stato di utilizzo del pulsante
-  digitalWrite(LED, iBut);
+  digitalWrite ( LED, iBut );
 }
